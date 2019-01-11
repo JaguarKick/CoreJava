1e4c0fe72db414bdef36e10713a5267c180be18d
tigerbeetle <jeremiah.4u@gmail.com>
2019-01-12 03:10:42 +0530

    Update

--- Basics/IfDemo.java
+++
@@ -1,9 +0,0 @@
-public class IfDemo{
-	public static void main(String args[]){
-		int age = 19;
-		if(age <= 18)
-			System.out.println("You're not an adult.");
-		else
-			System.out.println("You're an adult.");
-	}
-}
