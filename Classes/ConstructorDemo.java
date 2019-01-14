577b679fa4c029a0ce5c980cd116ce8868578051
tigerbeetle <jeremiah.4u@gmail.com>
2019-01-14 09:31:58 +0530

    Jan-14-2019

---
+++ Classes/ConstructorDemo.java
@@ -0,0 +1,11 @@
+class Abc{
+	Abc(){
+		System.out.println("I am a constructor.");
+	}
+}
+
+public class ConstructorDemo{
+	public static void main(String args[]){
+		new Abc();
+	}
+}
---
+++ Classes/ThisDemo.java
@@ -0,0 +1,25 @@
+class Student{
+	private int rollNo;
+	private String name;
+	
+	Student(String name, int rollNo){
+		this.name = name;
+		this.rollNo = rollNo;
+	}
+	
+	public int getRollNo(){
+		return rollNo;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
+
+public class ThisDemo{
+	public static void main(String args[]){
+		Student stud = new Student("Sam",45);
+		System.out.println(stud.getName());
+		System.out.println(stud.getRollNo());
+	}
+}
