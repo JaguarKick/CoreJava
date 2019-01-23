628ee165a8878d8e5c2917c3cc386e8fc03c1abd
tigerbeetle <jeremiah.4u@gmail.com>
2019-01-23 07:43:20 +0530

    Jan-23

---
+++ Basics/ParsingDemo.java
@@ -0,0 +1,6 @@
+public class ParsingDemo{
+	public static void main(String args[]){
+		char ch = 'a';
+		System.out.println((int)ch);
+	}
+}
---
+++ Enums/EnumDemo6.java
@@ -0,0 +1,7 @@
+class EnumDemo6{
+	enum SIZE{SMALL, MEDIUM, LARGE};
+	public static void main(String args[]){
+		SIZE size = SIZE.LARGE;
+		System.out.println(size);
+	}
+}
